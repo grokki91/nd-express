801ad820e25ef5ff9d76f8242c455503d75d68c0
Pavel Chekalin <grokki91@gmail.com>
2023-06-18 21:57:49 +0300

    add mongodb query in readme

--- Book.js
+++ Book.js
@@ -1,7 +1,7 @@
 const {v4: uuid} = require('uuid')
 
 class Book {
-    constructor(title = '', description = '', authors = '', favorite = false, fileCover = '', fileName = '', fileBook = '', count = 0, id = uuid()) {
+    constructor(title = '', description = '', authors = '', favorite = false, fileCover = '', fileName = '', fileBook = '_', count = 0, id = uuid()) {
         this.id = id
         this.title = title
         this.description = description
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# nd-express
+## Query for MongoDB:
+
+**add several books** <br> ```db.books.insertMany([{title: "Властелин Колец", description: "Фэнтези", authors: "Дж. Толкин"}, {title: "Дракула", description: "Ужасы", authors: "Б. Стокер"}])```
+
+**find books by title** <br> ```db.books.find({title: "Дракула"})```
+
+**update fields by id** <br> ```db.books.updateOne({"_id" : 1}, {$set: {description: "Война и мир", authors: "Л. Толстой"}})```

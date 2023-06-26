f1e0e0463de138cc8cd5769b89ef19d37d7ca56a
Pavel Chekalin <grokki91@gmail.com>
2023-06-26 20:17:17 +0300

    Connect to mongo

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express')
+const mongo = require('mongoose')
 const app = express()
 const routes = require('./routes')
 const api = require('./routes/api')
+require('dotenv').config()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 app.use('/public', express.static(__dirname+'/public'))
 app.use('/', routes)
-app.use('/', api)
+app.use('/', api);
 
+async function start(url, PORT) {
+    try {
+        await mongo.connect(url)
+        app.listen(PORT)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const url = process.env.URL;
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-    console.log(`Server listening post ${PORT}`);
-})
+start(url, PORT)

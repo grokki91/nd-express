8066672c9945c1097156f5f808d5659d8681ec9a
Pavel Chekalin <grokki91@gmail.com>
2023-08-06 21:16:34 +0300

    Add chat at view page

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const routes = require('./routes')
 const api = require('./routes/api')
+const http = require('http')
+const socketIO = require('socket.io')
+
+const server = http.Server(app)
+const io = socketIO(server)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -10,7 +15,27 @@ app.use('/public', express.static(__dirname+'/public'))
 app.use('/', routes)
 app.use('/', api)
 
+io.on('connection', (socket) => {
+    const {id} = socket
+    console.log(`Connection - ${id}`);
+
+    socket.on('message-to-me', (msg) => {
+        msg.type = 'me'
+        socket.emit('message-to-me', msg)
+    })
+
+    socket.on('message-to-all', (msg) => {
+        msg.type = `User-${id}`
+        socket.broadcast.emit('message-to-all', msg)
+        socket.emit('message-to-all', msg)
+    })
+
+    socket.on('disconnect', () => {
+        console.log(`Disconnect - ${id}`);
+    })
+})
+
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server listening post ${PORT}`);
 })

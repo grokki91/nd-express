d7b26e3e6670dad2469c5592cbf8ad0d6e7969f9
Pavel Chekalin <grokki91@gmail.com>
2023-09-27 20:51:23 +0300

    Book.ts add changes

--- Book.ts
+++ Book.ts
@@ -1,6 +1,6 @@
 const {v4: uuid} = require('uuid')
 
-interface Book {
+interface IBook {
     id: string,
     title: string,
     description: string,
@@ -12,11 +12,11 @@ interface Book {
 }
 
 abstract class BooksRepository {
-    createBook(book: Book[]): void {
+    createBook(book: IBook): void {
         
     } 
 
-    getBook(id: string): Book {
+    getBook(id: string): IBook {
         return
     }
 
@@ -33,4 +33,34 @@ abstract class BooksRepository {
     }
 }
 
-module.exports = BooksRepository
+class Book implements IBook {
+    public id: string
+    public title: string
+    public description: string 
+    public authors: string
+    public favorite: boolean
+    public fileCover: string
+    public fileName: string
+    public fileBook: string
+
+    constructor(
+        title: string,
+        description: string, 
+        authors: string,
+        favorite: boolean,
+        fileCover: string,
+        fileName: string,
+        fileBook: string,
+        id: string = uuid()
+    ) {
+        this.title = title
+        this.description = description, 
+        this.authors = authors,
+        this.favorite = favorite,
+        this.fileCover = fileCover,
+        this.fileName = fileName,
+        this.fileBook = fileBook,
+    }
+}
+
+module.exports = Book
